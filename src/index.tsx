6040726a4cf82d7d863727c25dd29fc289680215
SlavaMryasov <slavadevbox1@gmail.com>
2024-03-16 13:56:33 +0300

    1-3_exams

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,738 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// type UserPropsType = {
+//   name: string
+//   description: string
+// }
+// export const User: React.FC<UserPropsType> = (props) => {
+//   return <div>
+//       <h1>Имя: {props.name}</h1>
+//       //<div>Описание: {props.description}</div>
+//   </div>
+// }
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// //Что нужно написать вместо ххх, что бы код работал?
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+
+//22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+//22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+//22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+//22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string) => <li>{user}</li>
+//     const usersList = users.map(getUser)
+
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {usersList}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: Array.isArray(usersList)?
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string) => <li>{user}</li>
+//     const usersList = users.map(getUser)
+
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {usersList}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+
+// // Какую переменную необходимо указать вместо XXX, чтобы код заработал?
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+// console.log(typeof useState)
+//     return (
+//        <p>Тут будет список пользователей!</p>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList />, document.getElementById('root')
+// );
+
+// // Что вернёт выражение: typeof useState?
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string) => <li>{user}</li>
+//     console.log(typeof getUser)
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {users.map(getUser)}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: typeof getUser?
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+//     console.log(typeof setUsers)
+//     return (
+//        <p>Тут будет список пользователей</p>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: typeof setUsers?
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const results = useState<string[]>(["Bob", "Alex", "Ann"])
+//     const users = results[0]
+//     const setUsers = results[1]
+
+//     return (
+//        <p>Тут будет список пользователей</p>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+
+// Какой тип правильнее указать вместо "any" при типизации стэйта?
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// function User(props: UserType) {
+//     return (
+//         <li>User {props.name}: {props.age} y.o.</li>
+//     )
+// }
+
+// function UsersList() {
+//     const state = [
+//         {id: 1, name: "Bob", age: 34},
+//         {id: 2, name: "Alex", age: 25},
+//         {id: 3, name: "Ann", age: 30},
+//         {id: 4, name: "John", age: 23},
+//     ]
+//     const users = [
+//         {id: 1, userName: "Bob", age: 34},
+//         {id: 2, userName: "Alex", age: 25},
+//         {id: 3, userName: "Ann", age: 30},
+//         {id: 4, userName: "John", age: 23},
+//     ]
+    
+//     const [usersList, setUsersList] = useState<Array<UserType>>(state)
+//     return (
+//         <main>
+//             <h5>User list:</h5>
+//             <p>Тут будет список пользователей</p>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо написать вместо XXX, чтобы код работал?
+// // ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// // ❗ Ответ дать минимально возможным объёмом кода
+
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+// console.log(Array.isArray(users))
+//     return (
+//        <p>Тут будет список пользователей</p>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: Array.isArray(users)?
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const results = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+//     const users = results[0]
+//     const setUsers = results[1]
+// console.log(results)
+//     return (
+//        <p>Тут будет список пользователей</p>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+
+// // Чему равно results.length?
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                  { users.map(user => <li>{user}</li> )}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо вставить вместо ХХХ, чтобы код заработал?
+
+
+
+
+
+
+//33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+//33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+//33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+//33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+//33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// type UserPropsType = UserType & {
+//     deleteUser: (id: number) => void
+// }
+
+// function User(props: UserPropsType) {
+//     const deleteUser = () => props.deleteUser(props.id)
+//     return (
+//         <li>
+//             <button onClick={deleteUser}>x</button>
+//             User {props.name}: {props.age} y.o.
+//         </li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     const deleteUser = (userID: number) => {
+//         const filteredUsers = users.filter(u => u.id !== userID)
+//         setUsers(filteredUsers)
+//     }
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {users.map(u => <User
+//                     key={u.id}
+//                     {...u}
+//                     deleteUser={deleteUser}
+//                 />)}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо написать вместо xxx, чтобы код работал?
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// function User(props: UserType) {
+//     return (
+//         <li>Student {props.name}: {props.age} y.o.</li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 34},
+//         {id: 2, name: "Alex", age: 29},
+//         {id: 3, name: "Ann", age: 25},
+//         {id: 4, name: "John", age: 36},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 { users.map(u => <User key={u.id} {...u} />) }
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо написать вместо xxx, чтобы код работал?
+
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// function User(props: UserType) {
+//     return (
+//         <li>User {props.name}: {props.age} y.o.</li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     // Пользователи старше 25 лет:
+//     const olderThen25Users = users.filter(u => u.age > 25)
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо написать вместо xxx, чтобы код работал?
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function Button() {
+//     return <button
+//         onClick={()=> alert('')}
+//     >Click</button>
+// }
+
+
+// ReactDOM.render(
+//     <Button/>, document.getElementById('root')
+// );
+
+
+
+
+
+// // Что надо написать вместо ххх,
+// // что бы на странице появился пустой alert при клике по кнопке?
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// function User(props: UserType) {
+//     return (
+//         <li>User {props.name}: {props.age} y.o.</li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     // Необходимо отрендерить список ользователей старше 25 лет:
+//     const getOlderThen25Users = (u: UserType) => u.age > 25
+//     const olderThen25Users = users.filter(getOlderThen25Users)
+//     console.log(Array.isArray(olderThen25Users))
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: Array.isArray(olderThen25Users)
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// function User(props: UserType) {
+//     return (
+//         <li>User {props.name}: {props.age} y.o.</li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     // Необходимо отрендерить список пользователей старше 25 лет:
+//     const getOlderThen25Users = (u: UserType) => u.age > 25
+//     const olderThen25Users = users.filter(getOlderThen25Users)
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+
+// // Что надо написать вместо xxx, чтобы код работал?
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// function User(props: UserType) {
+//     return (
+//         <li>Student {props.name}: {props.age} y.o.</li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 34},
+//         {id: 2, name: "Alex", age: 29},
+//         {id: 3, name: "Ann", age: 25},
+//         {id: 4, name: "John", age: 36},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {
+//                    users.map(u => <User key={u.id}
+//                                         id={u.id}
+//                                         name={u.name}
+//                                         age={u.age}
+//                        />
+//                    )
+//                 }
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо написать вместо xxx, чтобы код работал оптимально?
+
+
+
+
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// type UserPropsType = UserType & {
+//     deleteUser: (id: number) => void
+// }
+
+// function User(props: UserPropsType) {
+//     return (
+//         <li>
+//             <button onClick={()=>props.deleteUser(props.id)}>x</button>
+//             User {props.name}: {props.age} y.o.
+//         </li>
+//     )
+// }
+
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     const deleteUser = (userID: number) => {
+//         setUsers(users.filter(u => u.id !== userID))
+//     }
+//     return (
+//         <main>
+//             <h4>Users list:</h4>
+//             <ul>
+//                 {users.map(u => <User
+//                     key={u.id}
+//                     {...u}
+//                     deleteUser={deleteUser}
+//                 />)}
+//             </ul>
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // В типе UserPropsType у функции deleteUser в параметрах указан тип "any".
+// // Какой тип было бы указать правильнее?
+
+
+
+
+
+
+// const user = {}
+// const boy = user
+// const student = {}
+
+
+// if(boy === user) {
+//     console.log ("Yo!!!")
+// }
+
+// /*Какую переменную надо указать вместо XXX, кроме user, что бы в консоль вывелась строка "Yo!!!?*/
+
+
+
+
+
+
+// import React, {useState, MouseEvent} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function Button() {
+//     const [tagName, setTagName] = useState<string>()
+//     const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+//         setTagName(e.currentTarget.tagName)
+//     }
+//     return (
+//         <>
+//             <p>{tagName}</p>
+//             <button onClick={onClickHandler} >
+//                 <span>Click</span>
+//             </button>
+//         </>
+//     )
+// }
+
+// ReactDOM.render(
+//     <Button/>, document.getElementById('root')
+// );
+
+// // Что надо написать вместо ххх, что бы на странице появился текст BUTTON?
+
+
+
+
+
+
+// import React, {MouseEvent} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function Button() {
+//     const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+//         console.log((typeof e) === 'object')
+//     }
+//     return <button onClick={onClickHandler} >Click</button>
+// }
+
+
+// ReactDOM.render(
+//     <Button/>, document.getElementById('root')
+// );
+
+// // Какой тип данных представляет аргумент функции-обработчика?
+// // Что надо написать вместо ххх, что бы в консоль вывело true?
+
