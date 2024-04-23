c259c6d824b7f9d4c09bf6b5616b4b8c0d3a0bfd
SlavaMryasov <slavadevbox1@gmail.com>
2024-04-23 16:32:55 +0300

    2.4 + total 2

--- src/index.tsx
+++ src/index.tsx
@@ -2474,3 +2474,572 @@
 // // и возвращает массив с именами общих друзей,
 // //если они есть и пустой массив, если таковых нету.
 // // Что надо написать вместо ххх, чтобы функция работала?
+
+
+
+
+
+
+
+//888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+//888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+//888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+//888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+//888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+//888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+
+
+// let result = 0
+
+// for(let i = 0; i < 5; i++){
+//     if(i % 2){
+//         result += i
+//     }
+// }
+
+// const studentName = (result - 4)
+//     ? "Michail"
+//     : "Elizaveta"
+
+// /* Какое значение получит переменная studentName? */ 
+// ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// const name = "Ekaterina"
+
+// const student = {
+//     [name]: name
+// }
+
+// const number = student["name"]
+//     ? 0.94
+//     : 34.48
+
+// /*Какое значение получит переменная number?*/
+
+
+
+
+// type ActionType = {
+//     type: "SUM" | "SUB" | "MULT" | "DIV" | "EXP"
+//     payload: number
+// }
+
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "MULT":
+//             return state * action.payload
+//         case "EXP":
+//             return state ** action.payload
+//         default:
+//             return state
+//     }
+// }
+// const result = calculator(10, { type: 'EXP', payload: 0 })
+// if (!(result - 1)) {
+//     console.log("IT-INCUBATOR")
+// }
+
+// //Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
+
+
+
+
+// type UserType = {
+//     id: number
+//     userName: string
+//     email: string
+//     password: string
+// }
+
+// type ChangeUserPasswordTypeAT = {
+//     type: "CHANGE-USER-PASSWORD"
+//     payload: {
+//         id: number
+//         newPassword: string
+//     }
+// }
+
+// export const userReducer =
+//     (state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
+//         switch (action.type) {
+//             case "CHANGE-USER-PASSWORD":
+//                 return state.map(u =>
+//                     u.id === action.payload.id
+//                         ? { ...u, password: action.payload.newPassword }
+//                         : u)
+//             default:
+//                 return state
+//         }
+//     }
+
+// //Какой код должен быть написан вместо XXX и YYY в типе //ChangeUserPasswordTypeAT, что бы редьюсер работал?
+// //В ответе напишите через пробел: XXX  YYY
+
+
+
+
+
+
+// type ActionType = {
+//     type: "SUM" | "SUB" | "MULT" | "DIV"
+//     payload: number
+// }
+
+// export const calculator = (state: number, action: ActionType) => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         default:
+//             return state
+//     }
+// }
+
+// //Что надо написать вместо ХХХ, чтобы функция calculator работала?
+
+
+
+
+// type ActionType = {
+//     type: "SUM" | "SUB" | "MULT" | "DIV" | "EXP"
+//     payload: number
+// }
+
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "EXP":
+//             return state ** action.payload
+//         default:
+//             return state
+//     }
+// }
+
+// console.log(calculator)
+// //Что вернёт такой вызов функции: calculator(10, {type: "MULT", payload: 2})?
+
+
+
+
+// const { beginning } = { beginning: 46 }
+
+// const multiply = beginning * 89.73
+
+// const randomValue = multiply && "Elena" && "Leo"
+
+// /* Какое значение получит переменная randomValue ? */
+
+// console.log(randomValue)
+
+
+
+
+// type ActionType = {
+//     type: "SUM" | "SUB" | "MULT" | "DIV"
+//     payload: number
+// }
+
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "MULT":
+//             return state * action.payload
+//         default:
+//             return state
+//     }
+// }
+
+// const result = calculator(10, { type: 'SUB', payload: 5 })
+// console.log(result)
+// //Что надо написать вместо XXX, что бы переменная result содержала значение 5?
+
+
+
+
+// const birth = {
+//     name: "Vladimir",
+//     scores: 54
+// }
+
+// const apartment = {
+//     ... birth,  scores:  birth.scores++
+// }
+
+
+// const scores  = apartment.scores
+
+// // Какое значение получит переменная scores?
+
+
+
+
+//TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2   
+//TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2   
+//TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2   
+//TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2   
+//TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2   
+//TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2 TOTAL2   
+
+
+
+
+// type StudentType = {
+//     id: number
+//     name: string
+// }
+
+// type FriendsType = {
+//     [key: string]: Array<String>
+// }
+
+// export const students: Array<StudentType> = [
+//     { id: 1, name: "Bob" },
+//     { id: 2, name: "Alex" },
+//     { id: 3, name: "Ann" },
+//     { id: 4, name: "Charley" },
+// ]
+
+// export const friends: FriendsType = {
+//     1: ["Oliver", "Jack", "Oscar",],
+//     2: ["Jack", "Lewis", "Thomas",],
+//     3: ["William", "Michael", "Lewis",],
+//     4: ["Oscar", "James", "William",],
+// }
+
+// console.log(friends[students[0].id][3])
+
+// //Дан список студентов и структура,
+// //которая содержит список друзей каждого из студентов.
+// //Id студента является ключом к массиву его друзей.
+// //Какое значение лежит тут: friends[students[0].id][3]?
+
+
+
+
+
+// import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function PasswordChecker() {
+//     const [password, setPassword] = useState<string>("")
+//     return (
+//         <main>
+//             <p>Your password must have more than 8 charters!</p>
+//             <input
+//                 placeholder={"Enter your password"}
+//                 value={password}
+//                 onChange={e => setPassword(e.currentTarget.value)}
+//                 type={"password"}
+//             />
+//             {password.length < 9 && <p style={{ color: "red" }}>The password is too short!</p>}
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <PasswordChecker />, document.getElementById('root')
+// );
+
+// // Что надо вставить вместо XXX, чтобы код работал нормально?
+
+
+
+
+
+
+// type ActionType = {
+//     type: "SUM" | "SUB" | "MULT" | "DIV" | "EXP"
+//     payload: number
+// }
+
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "MULT":
+//             return state * action.payload
+//         case "EXP":
+//             return state ** action.payload
+//         default:
+//             return state
+//     }
+// }
+// const result = calculator(10, { type: 'EXP', payload: 0 })
+// if (!(result - 1)) {
+//     console.log("IT-INCUBATOR")
+// }
+
+// //Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
+
+
+
+
+
+// type ActionType = {
+//     type: "SUM" | "SUB" | "MULT" | "DIV"
+//     payload: number
+// }
+
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "MULT":
+//             return state * action.payload
+//         default:
+//             return state
+//     }
+// }
+
+// const result = calculator(10, { type: 'SUB', payload: 5 })
+// console.log(result)
+// //Что надо написать вместо XXX, что бы переменная result содержала значение 5?
+
+
+
+
+
+// import React, { useState } from "react";
+
+// type UserType = {
+//     id: number
+//     name: string
+//     status: "online" | "offline"
+// }
+
+// type AddressType = {
+//     country: string
+//     city: string
+//     email: string
+// }
+
+// type AdressesType = {
+//     [userID: string]: AddressType
+// }
+
+// const users: Array<UserType> = [
+//     { id: 1, name: "Bob", status: "online" },
+//     { id: 2, name: "Alex", status: "offline" },
+//     { id: 3, name: "Ann", status: "offline" },
+// ]
+
+// const addresses: AdressesType = {
+//     1: { country: "Russia", city: "Moscow", email: "hey@email.com" },
+//     2: { country: "Ukraine", city: "Kiev", email: "yo@send.ua" },
+//     3: { country: "Belarus", city: "Minsk", email: "wow@gogo.ru" },
+
+// }
+
+// const updateUserAddress = (userID: number, key: string, newValue: string) => {
+//     return {
+//         ...addresses,
+//         [userID]: { ...addresses[userID], [key]: newValue }
+//     }
+// }
+// console.log(updateUserAddress(1, 'd', 'd'))
+// // Дан список пользователей и структура, хранящая адреса пользователей.
+// // Так же дана функция updateUserAddress,
+// // которая обновляет указанное в параметрах поле в адресе пользователя.
+// // Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
+// // Что надо написать вместо ххх, чтобы функция работала корректно?
+
+
+
+
+
+// type UserType = {
+//     id: number
+//     userName: string
+//     email: string
+//     password: string
+// }
+
+// type ChangeUserPasswordTypeAT = {
+//     type: "CHANGE-USER-PASSWORD"
+//     payload: {
+//         id: number
+//         newPassword: string
+
+//     }
+// }
+
+// export const userReducer =
+//     (state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
+//         switch (action.type) {
+//             case "CHANGE-USER-PASSWORD":
+//                 return state.map(u =>
+//                     u.id === action.payload.id
+//                         ? { ...u, password: action.payload.newPassword }
+//                         : u)
+//             default:
+//                 return state
+//         }
+//     }
+
+// //Какой код должен быть написан вместо XXX и YYY в типе //ChangeUserPasswordTypeAT, что бы редьюсер работал?
+// //В ответе напишите через пробел: XXX  YYY
+
+
+
+
+// type StudentType = {
+//     id: number
+//     name: string
+// }
+
+// type FriendsType = {
+//     [key: string]: Array<string>
+// }
+
+// export const students: Array<StudentType> = [
+//     { id: 1, name: "Bob" },
+//     { id: 2, name: "Alex" },
+//     { id: 3, name: "Ann" },
+//     { id: 4, name: "Charley" },
+// ]
+
+// export const friends: FriendsType = {
+//     1: ["Oliver", "Jack", "Oscar",],
+//     2: ["Jack", "Lewis", "Thomas",],
+//     3: ["William", "Michael", "Lewis",],
+//     4: ["Oscar", "James", "William",],
+// }
+
+// //Дан список студентов и структура,
+// //которая содержит список друзей каждого из студентов.
+// //Id студента является ключом к массиву его друзей.
+// //Какое значение лежит тут:  
+// console.log(friends[3][1])
+
+
+
+
+// import React, { useState, MouseEvent } from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function ColorButton() {
+//     const [isColored, setIsColored] = useState<boolean>(false)
+//     return (
+//         <button
+//             style={{ backgroundColor: `${isColored === true ? "red" : ""}` }}
+//             onClick={() => setIsColored(true)}
+//         >
+//             Меняю цвет по клику
+//         </button>
+//     )
+// }
+
+
+// ReactDOM.render(
+//     <ColorButton />, document.getElementById('root')
+// );
+
+// // Что надо написать вместо XXX, чтобы при клике кнопка становилась красной?
+
+
+
+
+
+// import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string, i: number) => <li key={i}>{user}</li>
+
+//     const usersList = (users.length === 0)
+//         ? <p>List is empty</p>
+//         : <ul>
+//             {users.map(getUser)}
+//         </ul>
+
+//     return (
+//         <main>
+//             <button onClick={() => setUsers([])}>Clear list</button>
+//             <h4>User list:</h4>
+//             {usersList}
+//         </main>
+//     )
+// }
+
+// ReactDOM.render(
+//     <UsersList />, document.getElementById('root')
+// );
+// // Что надо вставить вместо XXX, чтобы код корректно работал  со списком пользователей?
+
+
+
+
+
+type StudentType = {
+    id: number
+    name: string
+}
+
+type FriendsType = {
+    [key: string]: Array<string>
+}
+
+export const students: Array<StudentType> = [
+    { id: 1, name: "Bob" },
+    { id: 2, name: "Alex" },
+    { id: 3, name: "Ann" },
+    { id: 4, name: "Charley" },
+]
+
+export const friends: FriendsType = {
+    1: ["Oliver", "Jack", "Oscar",],
+    2: ["Jack", "Lewis", "Thomas",],
+    3: ["William", "Michael", "Lewis",],
+    4: ["Oscar", "James", "William",],
+}
+
+const getMutualFriends = (st_1: StudentType, st_2: StudentType,) => {
+    const result: Array<string> = []
+    friends[st_1.id].forEach(f => friends[st_2.id].includes(f) ? result.push(f) : null)
+    return result
+}
+
+//Дан массив студентов и структура,
+//которая содержит список друзей каждого из студентов.
+// Так же дана функция  getMutualFriends, проверяющая наличие общих друзей
+// у двух выбранных студентов.
+//Функция принимает параметром два объекта типа StudentType
+// и возвращает массив с именами общих друзей,
+//если они есть и пустой массив, если таковых нету.
+// Что надо написать вместо ххх, чтобы функция работала?
