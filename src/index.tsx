71a21b61ae5dd6fb9fe506de753c00459ae08a45
SlavaMryasov <slavadevbox1@gmail.com>
2024-05-16 19:18:16 +0300

    3.3

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@
 //   </div>
 // }
 
+
+
 // //Что нужно написать вместо ххх, что бы код работал?
 
 
@@ -3678,3 +3680,613 @@
 
 // // Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент
 // // удалялся из списка? Напишите через пробел.
+
+
+
+
+
+
+
+
+// import { useSelector } from 'react-redux'
+// import { combineReducers, legacy_createStore } from 'redux'
+
+// let initialState = { items: [{ name: 'Dimych' }, { name: 'Ignat' }] }
+// const usersReducer = (state = initialState, action: any) => {
+//     return state
+// }
+
+// const store = legacy_createStore(combineReducers({
+//     users: usersReducer
+// }))
+
+// store.subscribe(() => {
+//     const state = store.getState() 
+//     console.log(state)
+// })
+
+// store.dispatch({ type: 'ANY' })
+
+// //Что нужно написать вместо XXX, чтобы получить актуальный стейт?
+
+
+
+
+
+// import { combineReducers, createStore } from 'redux'
+
+// let initialState = { items: [{ name: 'Dimych' }, { name: 'Ignat' }] }
+// const usersReducer = (state = initialState, action: any) => {
+//     return state
+// }
+
+// const store = createStore(combineReducers({
+//     users: usersReducer
+// }))
+
+// store.getState().users.items.map(() => {
+//     console.log('state changed')
+// })
+
+// store.dispatch({ type: 'ANY' })
+
+// // Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'?
+
+
+
+
+// import React, { useState, useReducer, useEffect } from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// const changeCounter = (state: number, action: any): number => {
+//     switch (action.type) {
+//         case "INC_VALUE":
+//             return state + 1
+//         case "RESET":
+//             return 0
+//         case "DEC_VALUE":
+//             return state - 1
+//         default:
+//             return state
+//     }
+// }
+
+// function Counter() {
+//     const [value, setValue] = useReducer(changeCounter, 0)
+//     const [isCounter, setIsCounter] = useState(true)
+//     const commonStyles: React.CSSProperties = {
+//         border: "1px solid black",
+//         margin: "100px auto",
+//         width: "300px",
+//         height: "150px",
+//         textAlign: "center",
+//     }
+//     const btnStyles: React.CSSProperties = {
+//         color: "white",
+//         fontWeight: "bold",
+//         backgroundColor: "darkgray",
+//         borderRadius: "3px",
+//         minWidth: "40px"
+//     }
+
+//     return (
+//         <div style={commonStyles}>{
+//             isCounter
+//                 ? <div >
+//                     <div style={{ marginBottom: "20px" }}>
+//                         <h2>{value}</h2>
+//                         <button
+//                             style={{ ...btnStyles, backgroundColor: "red" }}
+//                             onClick={() => setIsCounter(false)}>OFF</button>
+//                     </div>
+//                     <button style={btnStyles} onClick={() => setValue({ type: "INC_VALUE" })}>+</button>
+//                     <button style={btnStyles} onClick={() => setValue({ type: "RESET" })}>0</button>
+//                     <button style={btnStyles} onClick={() => setValue({ type: "DEC_VALUE" })}>-</button>
+
+//                 </div>
+//                 : <div style={{ textAlign: "center" }}>
+//                     <h2>Counter not working</h2>
+//                     <button
+//                         style={{ ...btnStyles, backgroundColor: "green" }}
+//                         onClick={() => setIsCounter(true)}>ON</button>
+//                 </div>
+//         }
+//         </div>
+//     )
+// }
+
+
+// ReactDOM.render(
+//     <Counter />, document.getElementById('root')
+// );
+// // Что надо написать вместо XXX и YYY, чтобы код работал? Напишите через пробел.
+
+
+
+// import { combineReducers, createStore } from 'redux'
+// import ReactDOM from 'react-dom'
+// import { Provider, useSelector } from 'react-redux'
+// import React from 'react'
+
+// let initialState = {
+//     items:
+//         [
+//             { id: 1, name: 'Dimych' },
+//             { id: 2, name: 'Ignat' }
+//         ]
+// }
+// const usersReducer = (state = initialState, action: any) => {
+//     return state
+// }
+
+// let authInitialState = { login: 'Margo', settings: { theme: 'dark' } }
+// const authReducer = (state = authInitialState, action: any) => {
+//     return state
+// }
+
+// let rootReducer = combineReducers({
+//     users: usersReducer,
+//     auth: authReducer
+// })
+
+// const store = createStore(rootReducer)
+// type RootStateType = ReturnType<typeof rootReducer>
+
+// const selector = (state: RootStateType) => state.users.items
+
+// const Users = () => {
+
+//     const users = useSelector(selector)
+
+//     return <ul>
+//         {users.map(u => <li key={u.id}>{u.name}</li>)}
+//     </ul>
+// }
+
+// ReactDOM.render(<div>
+//     <Provider store={store}>
+//         <Users />
+//     </Provider>
+// </div>,
+//     document.getElementById('root')
+// )
+
+// // Что нужно написать вместо XXX, чтобы отрендерить список юзеров?
+// // ❗ Ответ дать минимально возможным объёмом кода 
+
+
+
+
+
+// const myFunction = (a: number, b = 3) => a + b;
+
+// const result = myFunction(346, undefined) % 2
+
+// const winner = result ? "Elizaveta" : "William"
+
+// //Какое значение получит переменная winner?
+
+
+
+// import React from 'react'
+// import { createStore } from 'redux'
+// import { Provider, useSelector, useDispatch } from 'react-redux'
+// import ReactDOM from 'react-dom'
+
+// type StudentType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+
+// const initState = {
+//     students:
+//         [
+//             { id: 1, name: 'Bob', age: 23 },
+//             { id: 2, name: 'Alex', age: 22 }
+//         ] as Array<StudentType>
+// }
+// type AddStudentAT = {
+//     type: 'ADD-STUDENT'
+//     name: string
+//     age: number
+//     id: number
+// }
+
+// type InitialStateType = typeof initState
+
+// const studentsReducer = (state: InitialStateType = initState, action: AddStudentAT): InitialStateType => {
+//     switch (action.type) {
+//         case 'ADD-STUDENT':
+//             return {
+//                 ...state,
+//                 students: [...state.students, {
+//                     name: action.name,
+//                     age: action.age,
+//                     id: action.id
+//                 }]
+//             }
+//     }
+//     return state
+// }
+
+// const appStore = createStore(studentsReducer)
+// type RootStateType = ReturnType<typeof studentsReducer>
+
+
+// const StudentList = () => {
+//     const students = useSelector((state: RootStateType) => state.students)
+//     return (
+//         <ul>
+//             {students.map(s => <li key={s.id}>{`${s.name}. ${s.age} years.`}</li>)}
+//         </ul>
+//     )
+// }
+// const App = () => {
+//     return <StudentList />
+// }
+
+// ReactDOM.render(<div>
+//     <Provider store={appStore}>
+//         <App />
+//     </Provider>
+// </div>,
+//     document.getElementById('root')
+// )
+
+// Что нужно написать вместо XXX, YYY и ZZZ, чтобы отобразился список студентов?
+
+
+
+
+// import { createStore } from 'redux'
+// import ReactDOM from 'react-dom'
+// import { Provider, useSelector, useDispatch } from 'react-redux'
+// import React from 'react'
+
+// const students = {
+//     students: [
+//         { id: 1, name: 'Bob' },
+//         { id: 2, name: 'Alex' },
+//         { id: 3, name: 'Donald' },
+//         { id: 4, name: 'Ann' },
+//     ]
+// }
+// type RemoveStudentAT = {
+//     type: "REMOVE-STUDENT"
+//     id: number
+// }
+// const RemoveStudentAC = (id: number): RemoveStudentAT => ({
+//     type: "REMOVE-STUDENT",
+//     id
+// })
+
+// const studentsReducer = (state = students, action: RemoveStudentAT) => {
+//     switch (action.type) {
+//         case "REMOVE-STUDENT":
+//             return {
+//                 ...state,
+//                 students: state.students.filter(s => s.id !== action.id)
+//             }
+//     }
+//     return state
+// }
+
+// const store = createStore(studentsReducer)
+// type RootStateType = ReturnType<typeof studentsReducer>
+
+
+// const StudentList = () => {
+//     const listItemStyles = {
+//         width: "100px",
+//         borderBottom: "1px solid gray",
+//         cursor: "pointer",
+//     }
+//     const students = useSelector((state: RootStateType) => state.students)
+//     const dispatch = useDispatch()
+//     const studentsList = students.map(s => {
+//         const removeStudent = () => {
+//             dispatch(RemoveStudentAC(s.id))
+//         }
+//         return (
+//             <li key={s.id}
+//                 style={listItemStyles}
+//                 onClick={removeStudent}>
+//                 {s.name}
+//             </li>)
+//     })
+//     return (
+//         <ol>
+//             {studentsList}
+//         </ol>
+
+//     )
+// }
+
+
+// ReactDOM.render(<div>
+//     <Provider store={store}>
+//         <StudentList />
+//     </Provider>
+// </div>,
+//     document.getElementById('root')
+// )
+
+// // Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент
+// // удалялся из списка? Напишите через пробел.
+
+
+
+
+// let number = 12
+
+// if (number > 0) {
+//     let number = 84
+//     number++
+// }
+
+// const getNumber = (number: any) => {
+//     number *= 10
+//     return number
+// }
+
+// const bigValue= getNumber("number") || number
+
+// //Какое значение получит переменная bigValue?
+
+
+
+
+//11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11  11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 
+//11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11  11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 
+//11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11  11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 
+//11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11  11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 
+//11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11  11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 
+//11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11  11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+// import { memo, useCallback, useState } from "react"
+
+// export const App = () => {
+//     const [temp, setTemp] = useState(100)
+//     const [seconds, setSeconds] = useState(0)
+
+//     const resetTemp = useCallback(() => setTemp(0), [])
+
+//     const incSec = () => setSeconds(state => state + 1)
+
+//     return <>
+//         <TempDisplay temp={temp} resetTemp={resetTemp} />
+//         <SecDisplay seconds={seconds} incSec={incSec} />
+//     </>
+// }
+// const TempDisplay = memo((props: any) => {
+//     console.log('Render TempDisplay')
+//     return (
+//         <div style={{ marginBottom: '10px' }} onClick={props.reset}>
+//             <p>
+//                 <b>Температура: </b>{props.temp} &#176;
+//             </p>
+//             <button onClick={props.resetTemp}>Сбросить температуру к 0</button>
+//         </div>
+//     )
+// })
+
+// const SecDisplay = memo((props: any) => {
+//     console.log('Render SecDisplay')
+//     return (
+//         <div>
+//             <p><b>Секунды:</b> {props.seconds} c </p>
+//             <button style={{ marginRight: '20px' }}
+//                 onClick={props.incSec}>
+//                 Увеличить время на 1 секунду
+//             </button>
+//         </div>
+//     )
+// })
+
+// const root = ReactDOM.createRoot(
+//     document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+// reportWebVitals();
+
+// // Почему не корректно работает счетчик времени при нажатии на кнопку (срабатывает только 1 раз) ?
+// // Найдите в чем причина.
+// // Исправленную версию строки напишите в качестве ответа
+
+// // Пример ответа: const incSec = () => setSeconds(seconds + 1)
+
+
+
+
+// import React, { useCallback, useState } from 'react'
+// import ReactDOM from 'react-dom'
+
+// export const TempManager = () => {
+//     const [temp, setTemp] = useState(0)
+//     const [seconds, setSeconds] = useState(0)
+
+//     const resetTemp = useCallback(() => setTemp(0), [temp])
+//     const increaseSeconds = useCallback(() => setSeconds((prevSeconds) => prevSeconds + 100), []);
+
+//     return (
+//         <>
+//             <TempDisplay temp={temp} reset={resetTemp} />
+//             <div>
+//                 <p><b>Секунды:</b> {seconds} с</p>
+//                 <button onClick={increaseSeconds}>
+//                     Увеличить время на 100 секунд
+//                 </button>
+//             </div>
+//         </>
+//     )
+// }
+
+// const TempDisplay = React.memo((props: any) => {
+//     console.log('Render TempDisplay')
+//     return (
+//         <div>
+//             <p><b>Температура</b>: {props.temp} &#176;</p>
+//             <button onClick={props.reset}>Reset</button>
+//         </div>
+//     )
+// })
+
+// ReactDOM.render(<TempManager />, document.getElementById('root'))
+
+
+// //При увеличении времени (при клике на button) компонент TempDisplay 
+// //тоже перерисовывается. Эта перерисовка является избыточной.
+// //Найдите в чем причина лишних перерисовок.
+// //Исправленную версию строки напишите в качестве ответа.
+
+// //Пример ответа: const increaseSeconds = () => setSeconds(seconds + 100)
+
+
+
+
+
+// import React, { useCallback, useState } from 'react'
+// import ReactDOM from 'react-dom'
+
+// export const App = () => {
+//     const [temp, setTemp] = useState(10)
+//     const [seconds, setSeconds] = useState(100)
+
+//     const increaseSeconds = () => setSeconds(seconds + 10)
+//     const increaseTemp = useCallback(() => setTemp(state => state + 1), [])
+
+//     return <>
+//         <TempDisplay temp={temp} increaseTemp={increaseTemp} />
+
+//         <div>
+//             <b>Секунды :</b> {seconds} с
+//             <button style={{ marginLeft: '15px' }}
+//                 onClick={increaseSeconds}>
+//                 Увеличить на 10 секунд
+//             </button>
+//         </div>
+//     </>
+// }
+// const TempDisplay = React.memo((props: any) => {
+//     console.log('Render TempDisplay')
+//     return (
+//         <div style={{ marginBottom: '15px' }}
+//             onClick={props.reset}>
+//             <b>Температура:</b> {props.temp} &#176;
+//             <button style={{ marginLeft: '15px' }}
+//                 onClick={props.increaseTemp}>
+//                 Увеличить температуру на 1 градус
+//             </button>
+//         </div>
+//     )
+// })
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // Что надо написать вместо XXX для того, чтобы обязательно выполнялись 2 условия:
+// // 1) При нажатии на кнопку "Увеличить температуру на 1 градус" температура увеличивалась
+// // 2) Компонент TempDisplay не должен перерисовываться при нажатии на кнопку "Увеличить на 10 секунд"
+
+// // Пример ответа: useEffect(() => setCounter(count + 1), [count])
+
+
+
+// const nature = {
+//     name: "Vanya",
+//     age: 69,
+//     technologies: ["HTML", "", "REACT"]
+//   }
+
+//   const [classroom = "FOO", balance = "BOO", article = "DOO"] = nature.technologies
+
+//   if(!balance){
+//     alert("HEY")
+//   }
+
+// Какую переменную следует указать вместо XXX, что бы была вызвана функция alert?
+
+
+
+// import React, { useMemo, useState } from 'react'
+// import ReactDOM from 'react-dom'
+
+// type ButtonType = {
+//     id: number
+//     title: string
+//     forAdminOnly: boolean
+// }
+// const buttons: ButtonType[] = [
+//     { id: 1, title: 'delete', forAdminOnly: true },
+//     { id: 2, title: 'update', forAdminOnly: true },
+//     { id: 3, title: 'create', forAdminOnly: false },
+// ]
+
+// export const App = ({ isAdmin }: { isAdmin: boolean }) => {
+
+//     const [seconds, setSeconds] = useState(0)
+
+//     const increaseSeconds = () => setSeconds(seconds + 10)
+
+//     const correctButtons = useMemo(() => {
+//         return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
+//     }, [])
+
+//     return <>
+//         <ButtonsPanel buttons={correctButtons} />
+//         <div>
+//             <p>
+//                 <b>Секунды: {seconds}</b>
+//             </p>
+//             <button onClick={increaseSeconds}>
+//                 Увеличить на 10 секунд
+//             </button>
+//         </div>
+//     </>
+// }
+
+// const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
+//     console.log('Render ButtonsPanel')
+//     return (
+//         <div style={{ marginBottom: '15px' }}>
+//             <div style={{ marginBottom: '15px' }}>
+//                 <b>Панель с кнопками</b>
+//             </div>
+//             <div>
+//                 {props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
+//             </div>
+//         </div>
+//     )
+// })
+
+// ReactDOM.render(<App isAdmin={true} />, document.getElementById('root'))
+
+// // Что нужно написать вместо XXX и YYY,
+// // чтобы избавиться от лишнего перерендера компонента ButtonsPanel
+// // при нажатии на кнопку "Увеличить на 10 секунд" ?
+
+// // Ответ дайте через пробел: 111 222
+
+
+
+
+// const numbers = [56, 76, 49, 61]
+// const mapFunction = (el: number, index: number) => el * index
+// const mappedArray = numbers.map(mapFunction)
+
+// const myNumber = mappedArray[0] && mappedArray[mappedArray.length - 1]
+// console.log(myNumber)
+// const bigCount = 1 + myNumber
+
+// // Какое значение получит переменная bigCount?//
+
+// //0 76 98 183
