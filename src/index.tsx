f3016ca06a06ad84c1802fd4d818687c44650447
SlavaMryasov <slavadevbox1@gmail.com>
2024-05-21 21:59:21 +0300

    3.4

--- src/index.tsx
+++ src/index.tsx
@@ -4290,3 +4290,155 @@
 // // Какое значение получит переменная bigCount?//
 
 // //0 76 98 183
+
+
+
+
+
+
+//12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 
+//12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 
+//12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 
+//12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 
+//12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 //12 
+
+
+
+
+// import { combineReducers, createStore } from 'redux'
+// let initialState = { items: [{ name: 'Dimych' }, { name: 'Ignat' }] }
+// const usersReducer = (state = initialState, action: any) => {
+//     return state
+// }
+// let authInitialState = { login: 'Margo', settings: { theme: 'dark' } }
+// const authReducer = (state = authInitialState, action: any) => {
+//     return state
+// }
+// const store = createStore(combineReducers({
+//     users: usersReducer,
+//     auth: authReducer
+// }))
+// store.subscribe(() => {
+//     const login = store.getState().auth.login
+//     console.log(login)
+// })
+// store.dispatch({ type: 'ANY' })
+// export default store;
+// // Что нужно написать вместо XXX, чтобы в консоли увидеть 'Margo'?
+
+
+
+
+// export const reducer = (state: any, action: any) => {
+//     switch (action.type) {
+//         case 'TRACK-DELETED':
+//             return state.filter((track: any) => track.id !== action.trackId)
+//         default:
+//             return state
+//     }
+// }
+
+// const deleteTrackAC = (trackId: number) => ({ type: 'TRACK-DELETED', trackId: trackId })
+
+
+// const state = [
+//     { id: 12, likesCount: 10 },
+//     { id: 14, likesCount: 2 },
+//     { id: 100, likesCount: 0 }
+// ]
+
+// const newState = reducer(state, deleteTrackAC(14))
+// console.log(newState.length === 2)
+
+// // Что нужно написать вместо XXX, чтобы корректно удалить трек и в консоли увидеть true?
+
+
+
+
+// const value = 27
+
+// const getValue = (value: any) => {
+//     value += 35
+//     return value
+// }
+
+// const myResult = getValue("") || value
+
+// //Какое значение получит переменная myResult?
+
+
+
+
+
+// const students = [
+//     {name: "Bob"},
+//     {name: "Alex"},
+//     {name: "Donald"}
+// ] 
+// const filteredStudents = students.filter(s => s.name !== "Kate")
+// const lengthDiff = students.length - filteredStudents.length
+// const newValue = (lengthDiff && 16.19) || (61.89 && 48.71) 
+
+// //Какое значение получит переменная newValue?
+
+
+
+// export const reducer = (state: any, action: any) => {
+//     switch (action.type) {
+//         case 'USER-NAME-UPDATED':
+//             return { ...state, user: { name: action.name } }
+
+//         default:
+//             return state
+//     }
+// }
+
+// const updateUserNameAC = (name: string) => ({ type: 'USER-NAME-UPDATED', name })
+
+
+// const state = {
+//     count: 10,
+//     user: {
+//         name: 'Dimych',
+//         age: 18,
+//         isMarried: true,
+//         status: "offline"
+//     },
+//     books: ['you don\'t know JS']
+// }
+// const newState = reducer(state, updateUserNameAC('Dmitry'))
+
+// console.log(newState.user.name === 'Dmitry')
+// console.log(newState.books === state.books)
+// console.log(newState.user !== state.user)
+
+// //Что нужно написать вместо XXX, чтобы корректно обновить имя пользователя и в консоли увидеть:  true true true?
+
+
+
+
+// export const reducer = (state: any, action: any) => {
+//     switch (action.type) {
+//         case 'TRACK-ADDED':
+//             return {
+//                 ...state,
+//                 [action.trackId]: {
+//                     id: action.trackId, likesCount: 0
+//                 }
+//             }
+//         default:
+//             return state
+//     }
+// }
+
+// const addTrackAC = (trackId: number) => ({ type: 'TRACK-ADDED', trackId })
+
+// const state = {
+//     12: { id: 12, likesCount: 10 },
+//     14: { id: 14, likesCount: 2 },
+//     100: { id: 100, likesCount: 0 },
+// }
+// const newState = reducer(state, addTrackAC(300))
+// console.log(newState[300].likesCount === 0)
+
+// // Что нужно написать вместо XXX, чтобы в консоли увидеть true?
